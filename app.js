a85e90978f422140c7674ee3a492a5a451f72b09
Jerry Gao <gao@blackwright.com>
2017-04-03 17:39:01 -0400

    add adjectifier middleware

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var reqAdjectifier = require('./lib/reqAdjectifier');
 
 let expressHandlebars = require("express-handlebars");
 
@@ -38,6 +38,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(reqAdjectifier);
 
 app.use('/', index);
 
